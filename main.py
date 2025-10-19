df477cca0b312af314893f7c0187fec7c4948cca
FarrukhYuldoshev <codingmaestro.uz@gmail.com>
2025-10-19 09:37:46 +0500

    added readme.md

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 import uvicorn
 
 app = FastAPI()
+
+
+# if we need some actions in starting application
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
